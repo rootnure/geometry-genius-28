dfe0c605ce6fb31339c7bc82df92643dae14fe00
rootnure <nur.diu.2791@gmail.com>
2023-08-18 02:19:14 +0600

    Multiple calculation options functionality added for rhombus

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -41,13 +41,41 @@ function calculateParallelogramArea() {
 }
 
 function calculateRhombusArea() {
-    const side = getInputValueById("rhombus-side");
-    const angle = getInputValueById("rhombus-angle");
-    const angleToRadian = angle * Math.PI / 180;
-    const area = side * side * Math.sin(angleToRadian);
+    const rhombusAreaBy = document.querySelector('#rhombus-area-by').value;
+    let area = 0;
+    if(rhombusAreaBy === 'side-angle') {
+        const side = getInputValueById("rhombus-side");
+        const angle = getInputValueById("rhombus-angle");
+        if(isNaN(side) || isNaN(angle)) {
+            alert('Please provide values in numbers format only');
+            return;
+        }
+        const angleToRadian = angle * Math.PI / 180;
+        area = Math.pow(side, 2) * Math.sin(angleToRadian);
+    }
+    else if(rhombusAreaBy === 'diagonals') {
+        const d1 = getInputValueById("rhombus-diagonal-1");
+        const d2 = getInputValueById("rhombus-diagonal-2");
+        if(isNaN(d1) || isNaN(d2)) {
+            alert('Please provide values in numbers format only');
+            return;
+        }
+        area = 0.5 * d1 * d2;
+    }
+    else if(rhombusAreaBy === 'base-height') {
+        const base = getInputValueById("rhombus-base");
+        const height = getInputValueById("rhombus-height");
+        if(isNaN(base) || isNaN(height)) {
+            alert('Please provide values in numbers format only');
+            return;
+        }
+        area = base * height;
+    }
     
     setElementInnerTextById('rhombus-area', area.toFixed(2));
 }
+
+
 
 /* reuseable function --> reduce duplicate code */
 
@@ -63,4 +91,63 @@ function getInputValueById(fieldId) {
 function setElementInnerTextById(elementId, area) {
     const element = document.querySelector('#'+elementId);
     element.innerText = area;
+}
+
+// toggle element function
+function toggleElementById(elementId, isHidden) {
+    const element = document.querySelector('#'+elementId);
+    if(isHidden) {
+        element.classList.remove('block');
+        element.classList.add('hidden');
+    }
+    else {
+        element.classList.remove('hidden');
+        element.classList.add('block');
+    }
+}
+
+function toggleRhombusAreas() {
+    const selection = document.querySelector('#rhombus-area-by').value;
+    const calculateBtn = document.querySelector('#calculate-rhombus-area');
+
+    if(selection === 'select') {
+        calculateBtn.setAttribute('disabled', true);
+        setElementInnerTextById('rhombus-area', '...');
+        toggleElementById('area-formula-side-angle', true);
+        toggleElementById('area-formula-diagonals', true);
+        toggleElementById('area-formula-base-height', true);
+        toggleElementById('user-input-side-angle', true);
+        toggleElementById('user-input-diagonals', true);
+        toggleElementById('user-input-base-height', true);
+    }
+    else if(selection === 'side-angle') {
+        calculateBtn.removeAttribute('disabled');
+        setElementInnerTextById('rhombus-area', '...');
+        toggleElementById('area-formula-side-angle', false);
+        toggleElementById('area-formula-diagonals', true);
+        toggleElementById('area-formula-base-height', true);
+        toggleElementById('user-input-side-angle', false);
+        toggleElementById('user-input-diagonals', true);
+        toggleElementById('user-input-base-height', true);
+    }
+    else if(selection === 'diagonals') {
+        calculateBtn.removeAttribute('disabled');
+        setElementInnerTextById('rhombus-area', '...');
+        toggleElementById('area-formula-side-angle', true);
+        toggleElementById('area-formula-diagonals', false);
+        toggleElementById('area-formula-base-height', true);
+        toggleElementById('user-input-side-angle', true);
+        toggleElementById('user-input-diagonals', false);
+        toggleElementById('user-input-base-height', true);
+    }
+    else if(selection === 'base-height') {
+        calculateBtn.removeAttribute('disabled');
+        setElementInnerTextById('rhombus-area', '...');
+        toggleElementById('area-formula-side-angle', true);
+        toggleElementById('area-formula-diagonals', true);
+        toggleElementById('area-formula-base-height', false);
+        toggleElementById('user-input-side-angle', true);
+        toggleElementById('user-input-diagonals', true);
+        toggleElementById('user-input-base-height', false);
+    }
 }
