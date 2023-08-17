de18a29f37e23d6745ad5a1fd0a37367236746d1
rootnure <nur.diu.2791@gmail.com>
2023-08-17 23:32:25 +0600

    Rectangle area calculation added

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -14,3 +14,20 @@ function calculateTriangleArea() {
     const triangleArea = document.querySelector("#triangle-area");
     triangleArea.innerText = area;
 }
+
+function calculateRectangleArea() {
+    // get triangle base value
+    const heightField = document.querySelector('#rectangle-height');
+    const height = parseFloat(heightField.value);
+    
+    // get rectangle height value
+    const widthField = document.querySelector('#rectangle-width');
+    const width = parseFloat(widthField.value);
+    
+    // calculate rectangle area
+    const area = height * width;
+    
+    // show rectangle area
+    const rectangleArea = document.querySelector("#rectangle-area");
+    rectangleArea.innerText = area;
+}
