9ef16238be3d19bb23fd5f45e2fbc7d525af63fe
rootnure <nur.diu.2791@gmail.com>
2023-08-18 02:45:18 +0600

    Rhombus user input field type changed to number. Toggle Element modified using for-in loop

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -2,11 +2,11 @@ function calculateTriangleArea() {
     // get triangle base value
     const baseField = document.querySelector('#triangle-base');
     const base = parseFloat(baseField.value);
-    
+
     // get triangle height value
     const heightField = document.querySelector('#triangle-height');
     const height = parseFloat(heightField.value);
-    
+
     // calculate triangle area
     const area = 0.5 * base * height;
 
@@ -19,14 +19,14 @@ function calculateRectangleArea() {
     // get rectangle height value
     const heightField = document.querySelector('#rectangle-height');
     const height = parseFloat(heightField.value);
-    
+
     // get rectangle width value
     const widthField = document.querySelector('#rectangle-width');
     const width = parseFloat(widthField.value);
-    
+
     // calculate rectangle area
     const area = height * width;
-    
+
     // show rectangle area
     const rectangleArea = document.querySelector("#rectangle-area");
     rectangleArea.innerText = area;
@@ -36,42 +36,41 @@ function calculateParallelogramArea() {
     const base = getInputValueById("parallelogram-base");
     const height = getInputValueById("parallelogram-height");
     const area = base * height;
-    
+
     setElementInnerTextById('parallelogram-area', area);
 }
 
 function calculateRhombusArea() {
     const rhombusAreaBy = document.querySelector('#rhombus-area-by').value;
     let area = 0;
-    if(rhombusAreaBy === 'side-angle') {
+    if (rhombusAreaBy === 'side-angle') {
         const side = getInputValueById("rhombus-side");
         const angle = getInputValueById("rhombus-angle");
-        if(isNaN(side) || isNaN(angle)) {
-            alert('Please provide values in numbers format only');
+        if (isNaN(side) || isNaN(angle)) {
+            emptyOrStringMsg();
             return;
         }
         const angleToRadian = angle * Math.PI / 180;
         area = Math.pow(side, 2) * Math.sin(angleToRadian);
     }
-    else if(rhombusAreaBy === 'diagonals') {
+    else if (rhombusAreaBy === 'diagonals') {
         const d1 = getInputValueById("rhombus-diagonal-1");
         const d2 = getInputValueById("rhombus-diagonal-2");
-        if(isNaN(d1) || isNaN(d2)) {
-            alert('Please provide values in numbers format only');
+        if (isNaN(d1) || isNaN(d2)) {
+            emptyOrStringMsg();
             return;
         }
         area = 0.5 * d1 * d2;
     }
-    else if(rhombusAreaBy === 'base-height') {
+    else if (rhombusAreaBy === 'base-height') {
         const base = getInputValueById("rhombus-base");
         const height = getInputValueById("rhombus-height");
-        if(isNaN(base) || isNaN(height)) {
-            alert('Please provide values in numbers format only');
+        if (isNaN(base) || isNaN(height)) {
+            emptyOrStringMsg();
             return;
         }
         area = base * height;
     }
-    
     setElementInnerTextById('rhombus-area', area.toFixed(2));
 }
 
@@ -81,7 +80,7 @@ function calculateRhombusArea() {
 
 // reusable get input value field in number
 function getInputValueById(fieldId) {
-    const field = document.querySelector('#'+fieldId);
+    const field = document.querySelector('#' + fieldId);
     const fieldString = field.value;
     const value = parseFloat(fieldString);
     return value;
@@ -89,14 +88,14 @@ function getInputValueById(fieldId) {
 
 // reusable set span, p, div etc. text
 function setElementInnerTextById(elementId, area) {
-    const element = document.querySelector('#'+elementId);
+    const element = document.querySelector('#' + elementId);
     element.innerText = area;
 }
 
 // toggle element function
 function toggleElementById(elementId, isHidden) {
-    const element = document.querySelector('#'+elementId);
-    if(isHidden) {
+    const element = document.querySelector('#' + elementId);
+    if (isHidden) {
         element.classList.remove('block');
         element.classList.add('hidden');
     }
@@ -106,48 +105,70 @@ function toggleElementById(elementId, isHidden) {
     }
 }
 
+// empty or string value input message
+function emptyOrStringMsg() {
+    alert('Input fields cannot be empty or string');
+}
+
 function toggleRhombusAreas() {
     const selection = document.querySelector('#rhombus-area-by').value;
     const calculateBtn = document.querySelector('#calculate-rhombus-area');
+    setElementInnerTextById('rhombus-area', '...');
 
-    if(selection === 'select') {
+    if (selection === 'select') {
         calculateBtn.setAttribute('disabled', true);
-        setElementInnerTextById('rhombus-area', '...');
-        toggleElementById('area-formula-side-angle', true);
-        toggleElementById('area-formula-diagonals', true);
-        toggleElementById('area-formula-base-height', true);
-        toggleElementById('user-input-side-angle', true);
-        toggleElementById('user-input-diagonals', true);
-        toggleElementById('user-input-base-height', true);
+        const toggleElements = {
+            'area-formula-side-angle': true,
+            'area-formula-diagonals': true,
+            'area-formula-base-height': true,
+            'user-input-side-angle': true,
+            'user-input-diagonals': true,
+            'user-input-base-height': true,
+        }
+        for(const element in toggleElements) {
+            toggleElementById(element, toggleElements[element]);
+        }
     }
-    else if(selection === 'side-angle') {
+    else if (selection === 'side-angle') {
         calculateBtn.removeAttribute('disabled');
-        setElementInnerTextById('rhombus-area', '...');
-        toggleElementById('area-formula-side-angle', false);
-        toggleElementById('area-formula-diagonals', true);
-        toggleElementById('area-formula-base-height', true);
-        toggleElementById('user-input-side-angle', false);
-        toggleElementById('user-input-diagonals', true);
-        toggleElementById('user-input-base-height', true);
+        const toggleElements = {
+            'area-formula-side-angle': false,
+            'area-formula-diagonals': true,
+            'area-formula-base-height': true,
+            'user-input-side-angle': false,
+            'user-input-diagonals': true,
+            'user-input-base-height': true,
+        }
+        for(const element in toggleElements) {
+            toggleElementById(element, toggleElements[element]);
+        }
     }
-    else if(selection === 'diagonals') {
+    else if (selection === 'diagonals') {
         calculateBtn.removeAttribute('disabled');
-        setElementInnerTextById('rhombus-area', '...');
-        toggleElementById('area-formula-side-angle', true);
-        toggleElementById('area-formula-diagonals', false);
-        toggleElementById('area-formula-base-height', true);
-        toggleElementById('user-input-side-angle', true);
-        toggleElementById('user-input-diagonals', false);
-        toggleElementById('user-input-base-height', true);
+        const toggleElements = {
+            'area-formula-side-angle': true,
+            'area-formula-diagonals': false,
+            'area-formula-base-height': true,
+            'user-input-side-angle': true,
+            'user-input-diagonals': false,
+            'user-input-base-height': true,
+        }
+        for(const element in toggleElements) {
+            toggleElementById(element, toggleElements[element]);
+        }
     }
-    else if(selection === 'base-height') {
+    else if (selection === 'base-height') {
         calculateBtn.removeAttribute('disabled');
-        setElementInnerTextById('rhombus-area', '...');
-        toggleElementById('area-formula-side-angle', true);
-        toggleElementById('area-formula-diagonals', true);
-        toggleElementById('area-formula-base-height', false);
-        toggleElementById('user-input-side-angle', true);
-        toggleElementById('user-input-diagonals', true);
-        toggleElementById('user-input-base-height', false);
+        const toggleElements = {
+            'area-formula-side-angle': true,
+            'area-formula-diagonals': true,
+            'area-formula-base-height': false,
+            'user-input-side-angle': true,
+            'user-input-diagonals': true,
+            'user-input-base-height': false,
+        }
+        for(const element in toggleElements) {
+            toggleElementById(element, toggleElements[element]);
+        }
     }
 }
