4f6e0fcc55b3e81b91d39317fba138a3a4019c3b
rootnure <nur.diu.2791@gmail.com>
2023-08-18 15:35:26 +0600

    Ellipse functionality added

---
+++ js/calculate-ellipse-area.js
@@ -0,0 +1,11 @@
+function calculateEllipseArea() {
+    const majorRadius = getInputValueById('ellipse-major-radius');
+    const minorRadius = getInputValueById('ellipse-minor-radius');
+    if(isNaN(majorRadius) || isNaN(minorRadius)) {
+        emptyOrStringMsg();
+        return;
+    }
+    const area = Math.PI * majorRadius * minorRadius;
+    setElementInnerTextById('ellipse-area', parseFloat(area.toFixed(2)));
+    console.log(Math.PI);
+}
