c1ecbce71be0b79492400fb09e4dc3a63ed41721
rootnure <nur.diu.2791@gmail.com>
2023-08-19 07:29:47 +0600

    isNaN validation added for Rectangle & Parallelogram

--- js/calculate-parallelogram-area.js
+++ js/calculate-parallelogram-area.js
@@ -1,6 +1,10 @@
 function calculateParallelogramArea() {
     const base = getInputValueById("parallelogram-base");
     const height = getInputValueById("parallelogram-height");
+    if(isNaN(base) || isNaN(height)) {
+        emptyOrStringMsg();
+        return;
+    }
     const area = base * height;
 
     setElementInnerTextById('parallelogram-area', parseFloat(area.toFixed(2)));
--- js/calculate-rectangle-area.js
+++ js/calculate-rectangle-area.js
@@ -1,11 +1,15 @@
 function calculateRectangleArea() {
     // get rectangle height value
-    const heightField = document.querySelector('#rectangle-height');
-    const height = parseFloat(heightField.value);
+    const height = getInputValueById('rectangle-height');
 
     // get rectangle width value
-    const widthField = document.querySelector('#rectangle-width');
-    const width = parseFloat(widthField.value);
+    const width = getInputValueById('rectangle-width');
+
+    // isNaN validation check
+    if(isNaN(height) || isNaN(width)) {
+        emptyOrStringMsg();
+        return;
+    }
 
     // calculate rectangle area
     const area = height * width;
@@ -13,5 +17,6 @@ function calculateRectangleArea() {
     // show rectangle area
     setElementInnerTextById('rectangle-area', parseFloat(area.toFixed(2)));
 
+    // add result to history container
     addToCalculationEntry('Rectangle(H-W)', area);
 }
